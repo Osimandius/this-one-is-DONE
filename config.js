12ec1dedd3ebb28970e8cf33049d020eb82df2c5
Osimandius <60579181+Osimandius@users.noreply.github.com>
2021-11-11 11:12:08 -0600

    Add files via upload
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ config.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyASKyEjf7xunU5pHK2ZXl457dqXD2WjVhQ",
+  
+    authDomain: "storytelling1-640a0.firebaseapp.com",
+  
+    projectId: "storytelling1-640a0",
+  
+    storageBucket: "storytelling1-640a0.appspot.com",
+  
+    messagingSenderId: "730629087883",
+  
+    appId: "1:730629087883:web:21b3c8039ae04a8194aa8e"
+  
+  };
+  
+  
